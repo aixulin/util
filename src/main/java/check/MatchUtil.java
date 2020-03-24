bbb54e27263158a592a81bb70775c66a49a2881d
xulin <xulin@gosun.com>
2020-03-24 11:09:30 +0800

    补充手机号和邮箱的正则表达式

---
+++ src/main/java/check/MatchUtil.java
@@ -0,0 +1,22 @@
+package check;
+
+/**
+ * @author xulin
+ * @date 2020/3/23 17:49
+ * 此类用于存放一些常用的正则表达式子
+ */
+public class MatchUtil {
+    /**
+     * 手机号正则
+     * 会持续关注营业厅发布的号段 之后进行更新
+     */
+    public static final String REGEX_TEL = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0-8])|(18[0-9])|(19[1|8|9]))\\d{8}$";
+    /**
+     * 邮箱验证
+     */
+    public static final String REGEX_EMAIL = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+    // 密码正则
+
+
+}
