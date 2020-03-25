8e5594d8f372c8d38d9f89c6831760e8a6c47778
xulin <1658839983@qq.com>
2020-03-25 14:04:30 +0800

    补充正则校验 对文档进行修改更新

--- src/main/java/check/MatchUtil.java
+++ src/main/java/check/MatchUtil.java
@@ -19,10 +19,23 @@ public class MatchUtil {
      * 密码正则
      * 包含 大写字母 小写字母 数字 特殊符号 在8-16位之间
      */
-    public static final String REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[_.#?!@$%^&*-]).{8,16}$";
-
-
-
+    public static final String REGEX_PWD = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[_.#?!@$%^&*-]).{8,16}$";
+    /**
+     * ip地址校验
+     */
+    public static final String REGEX_IP = "(?=(\\b|\\D))(((\\d{1,2})|(1\\d{1,2})|(2[0-4]\\d)|(25[0-5]))\\.){3}((\\d{1,2})|(1\\d{1,2})|(2[0-4]\\d)|(25[0-5]))(?=(\\b|\\D))";
+    /**
+     * 邮政编码校验
+     */
+    public static final String REGEX_POST = "[1-9]d{5}(?!d)";
+    /**
+     * 身份证校验 identity 18位
+     */
+    public static final String REGEX_IDENTITY= "^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$";
+    /**
+     * 身份证校验 15位
+     */
+    public static final String REGEX_IDENTITY_15 = "^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}$";
 
 
 }
