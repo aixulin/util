74a11d1e2304d041534bd0049880f9b5838cea97
xulin <1658839983@qq.com>
2020-04-16 10:23:15 +0800

    提供一种方式可以直接获取系统中的response

--- src/main/java/request/RequestUtil.java
+++ src/main/java/request/RequestUtil.java
@@ -4,6 +4,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static check.CheckUtil.isEmpty;
 import static check.CheckUtil.isNotEmpty;
@@ -13,6 +14,7 @@ import static check.CheckUtil.isNotEmpty;
  * @date 2020/4/7 13:45
  */
 public class RequestUtil {
+
     /**
      * 获取用户的真实的请求的ip
      */
@@ -34,5 +36,16 @@ public class RequestUtil {
             return ip;
         }
         return request.getRemoteAddr();
+    }
+
+    /**
+     * 直接获取请求中的response
+     * <p>通常获取request的方式我们可以注入获取</p>
+     * <p>@Autowired private HttpServletRequest request;</p>
+     * <p>可是同样的方式获取response却不可以，下面提供一种方式用于获取response</p>
+     */
+    public static HttpServletResponse getResponse() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getResponse();
     }
 }
